7b1683df3132be2a1dbe7060bf2cdbb846251520
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2018-10-13 11:54:55 +0200

    Add files via upload

---
+++ PerfectRedac.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package PerfectRedac
+ * @version 0.0.1
+ */
+ /*
+ Plugin Name : PerfectRedac
+ Plugin URL :
+ Description : Correcteur orthographique de traitement de texte, spécialement conçu pour WordPress. 
+ Author : Valentindu62
+ Version : 0.0.1
+ Author URL :
+ */
+ 
